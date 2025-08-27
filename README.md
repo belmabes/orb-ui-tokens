6567880430aebf29b7ffeac7fdaa386bbfa7bb95
belma-b <github@belmab.dev>
2025-08-27 20:27:34 +0200

    first init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Design Tokens
+
+## Installation
+```bash
+npm install @orb-ui/design-tokens
---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  source: ['tokens.json'],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'dist/',
+      files: [{
+        destination: 'tokens.css',
+        format: 'css/variables',
+        options: {
+          outputReferences: true
+        }
+      }]
+    },
+    scss: {
+      transformGroup: 'scss',
+      buildPath: 'dist/',
+      files: [{
+        destination: 'tokens.scss',
+        format: 'scss/variables'
+      }]
+    },
+    js: {
+      transformGroup: 'js',
+      buildPath: 'dist/',
+      files: [{
+        destination: 'tokens.js',
+        format: 'javascript/es6'
+      }]
+    }
+  }
+};
